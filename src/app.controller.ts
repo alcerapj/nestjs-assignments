3017cdd8c6a1002032f8562d43303d5daf32959d
pj <pjalcera08@gmail.com>
2024-11-08 16:33:53 +0800

    Completed Assignment 2: Prime Number Checker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,4 +17,25 @@ export class AppController {
 
   }
 
+  @Get('prime/:n')
+  checkPrime(@Param('num') num: string): { isPrime: boolean } {
+    const number = parseInt(num, 10);
+    const isPrime = this.isPrime(number);
+    return { isPrime };
+  }
+
+  private isPrime(n: number): boolean {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 === 0 || n % 3 === 0) return false;
+
+    for (let i = 5; i * i <= n; i += 6) {
+      if (n % i === 0 || n % (i + 2) === 0) return false;
+    }
+
+    return true;
+  }
+  
+  
+
 }
